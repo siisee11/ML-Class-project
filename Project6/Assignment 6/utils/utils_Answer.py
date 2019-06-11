b4128dad0e6eac0f1738bacbf9d75fb87fd3222f
siisee11 <gch02103@naver.com>
2019-06-11 12:22:14 +0900

    Add files via upload

---
+++ Project6/Assignment 6/utils/utils_Answer.py
@@ -0,0 +1,75 @@
+import numpy as np
+
+def Gini_index(Y_data):
+    gini = 0
+    #=========    Edit here    ==========
+    ny = 0
+    nn = 0
+    for y in Y_data:
+        if y is 'yes' or 1.0:
+            ny += 1
+        else:
+            nn += 1
+
+    n = ny + nn
+
+    np.log2()
+    #====================================
+    return gini
+
+def Entropy(Y_data):
+
+    entropy = 0
+    # =====    Edit here    ========
+    #
+    #
+    #
+    # ==============================
+    return entropy
+
+def impurity_func(Y_data, criterion):
+
+    if criterion == 'gini':
+        return Gini_index(Y_data)
+
+    elif criterion == 'entropy':
+        return Entropy(Y_data)
+
+def Finding_split_point(df, feature, criterion):
+
+    col_data = df[feature]
+    Y = df.values[:, -1]
+    distinct_data = np.unique(col_data)
+
+    split_point = distinct_data[0]
+    min_purity = 1
+
+    for idx, val in enumerate(distinct_data):
+        less_idx = (col_data < val)
+
+        y0 = Y[less_idx]
+        y1 = Y[~less_idx]
+
+        p0 = len(y0) / len(Y)
+        p1 = len(y1) / len(Y)
+
+        purity = np.sum([p0 * impurity_func(y0, criterion), p1 * impurity_func(y1, criterion)])
+
+        if min_purity > purity:
+            min_purity = purity
+            split_point = val
+
+    return split_point
+
+def Gaussian_prob(x, mean, std):
+    '''
+    :param x: input value: X
+    :param mean: the mean of X
+    :param std: the standard deviation of X
+    :return: probaility (X) ~ N(μ, σ^2)
+    '''
+    ret = 0
+    # ========      Edit here         ==========
+
+    # =========================================
+    return ret
